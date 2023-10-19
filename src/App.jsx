491436356ec2e42189b4aff84e84be455eddbf50
Junior Becerra <junioralejandrobecerra@gmail.com>
2023-10-19 10:51:37 -0400

    terminando el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState, useEffect } from "react";
-import styled from "@emotion/styled";
-import ImagenCripto from "./img/imagen-criptos.png";
-import Formulario from "./components/Formulario";
+import { useState, useEffect } from 'react'
+import styled from '@emotion/styled'
+import Formulario from './components/Formulario'
+import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
+import ImagenCripto from './img/imagen-criptos.png'
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -12,18 +14,17 @@ const Contenedor = styled.div`
     grid-template-columns: repeat(2, 1fr);
     column-gap: 2rem;
   }
-`;
-
+`
 const Imagen = styled.img`
   max-width: 400px;
   width: 80%;
   margin: 100px auto 0 auto;
   display: block;
-`;
+`
 
 const Heading = styled.h1`
-  font-family: "lato", sans-serif;
-  color: #fff;
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
   text-align: center;
   font-weight: 700;
   margin-top: 80px;
@@ -31,48 +32,62 @@ const Heading = styled.h1`
   font-size: 34px;
 
   &::after {
-    content: "";
+    content: '';
     width: 100px;
     height: 6px;
-    background-color: #66a2fe;
+    background-color: #66A2FE;
     display: block;
     margin: 10px auto 0 auto;
   }
-`;
+` 
 
 function App() {
-  const [monedas, setMonedas] = useState({});
-  const [cotizacion, setCotizacion] = useState({});
+
+  const [ monedas, setMonedas ] = useState({})
+  const [ resultado, setResultado ] = useState({})
+  const [ cargando, setCargando ] = useState(false)
 
   useEffect(() => {
-    if (Object.keys(monedas).length > 0) {
-      console.log("hola");
+      if(Object.keys(monedas).length > 0) {
+          
+        const cotizarCripto = async () => {
+            setCargando(true)
+            setResultado({})
 
-      const cotizarCripto = async () => {
-        const { moneda, criptomoneda } = monedas;
+            const { moneda, criptomoneda } = monedas
+            const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
 
-        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+            const respuesta = await fetch(url)
+            const resultado = await respuesta.json()
 
-        const respuesta = await fetch(url);
-        const resultado = await respuesta.json();
-        setCotizacion(resultado.DISPLAY[criptomoneda][moneda]);
-      };
+            setResultado(resultado.DISPLAY[criptomoneda][moneda])
 
-      cotizarCripto();
-    }
-  }, [monedas]);
+            setCargando(false)
+        }
+
+        cotizarCripto()
+      }
+  }, [monedas])
 
   return (
-    <Contenedor>
-      <Imagen src={ImagenCripto} />
+      <Contenedor>
+          <Imagen 
+            src={ImagenCripto}
+            alt="imagenes criptomonedas"
+          />
 
-      <div>
-        <Heading>Cotiza criptomonedas al instante</Heading>
+          <div>
+              <Heading>Cotiza Criptomonedas al Instante</Heading>
+              <Formulario 
+                setMonedas={setMonedas}
+              />
 
-        <Formulario setMonedas={setMonedas} />
-      </div>
-    </Contenedor>
-  );
+              {cargando && <Spinner />}
+              {resultado.PRICE && <Resultado resultado={resultado} />} 
+          </div>
+
+      </Contenedor>
+  )
 }
 
-export default App;
+export default App
